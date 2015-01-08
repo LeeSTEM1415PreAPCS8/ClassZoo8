a12f58f917a857b84f22ea4da96665eb7fbe87ed
stijerina2282 <stijerina2282@001LAB102010553.neisd.ad>
2015-01-08 15:30:38 -0600

    added tiger

---
+++ ClassZoo8/src/tiger.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.applet.*;
+
+public class tiger extends Applet{
+	
+	public void paint(Graphics g)
+	{
+		drawAnimal(g);
+		drawHabitat(g);
+	}
+
+	private void drawAnimal(Graphics g)
+	{
+		
+	}
+	
+	private void drawHabitat(Graphics g)
+	{
+		
+	}
+}
