470e24630642b2cac7d834ad7374b80ecb7e42ee
crodriguezc0697 <crodriguezc0697@001LAB102010544.neisd.ad>
2015-01-08 15:46:00 -0600

    name

---
+++ ClassZoo8/src/monkey.java
@@ -0,0 +1,24 @@
+charlie rodriguez
+
+
+import java.awt.*;
+import java.applet.*;
+
+public class monkey extends Applet{
+	
+	public void paint(Graphics g)
+	{
+		drawAnimal(g);
+		drawHabitat(g);
+	}
+
+	private void drawAnimal(Graphics g)
+	{
+		
+	}
+	
+	private void drawHabitat(Graphics g)
+	{
+		
+	}
+}
