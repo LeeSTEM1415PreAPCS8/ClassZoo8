4b700396cbbb283e78bbf30e5bb1898db5a6a75e
Collin Norwood <collinthegeek@gmail.com>
2015-01-08 15:52:57 -0600

    Made the Muntjac file do something

--- ClassZoo8/src/Munjac.java
+++ ClassZoo8/src/Munjac.java
@@ -1,1 +1,27 @@
-#This is the file for a Reeves Muntjac
+/*Computer Science Pre-AP
+8th period
+Collin Norwood
+*/
+
+import java.awt.*;
+import java.applet.*;
+
+public class Muntjac extends Applet{
+	
+	public void paint(Graphics g)
+	{
+		drawHabitat(g);
+		drawAnimal(g);
+	}
+
+	private void drawAnimal(Graphics g)
+	{	
+		g.setColor (Color.blue);
+		g.fillRect(10, 10, 10, 10);
+	}
+	
+	private void drawHabitat(Graphics g)
+	{
+		
+	}
+}
