5951f7fede2df3174eec8736642a1b2e74e4b10e
gskaggs2747 <gskaggs2747@001LAB102010543.neisd.ad>
2015-01-08 15:56:58 -0600

    added dragon lololol

---
+++ ClassZoo8/src/dragon.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.applet.*;
+
+public class  extends Applet{
+	
+	public void paint(Graphics g)
+	{
+		drawAnimal(g);
+		drawHabitat(g);
+	}
+
+	private void drawAnimal(Graphics g)
+	{
+		g.setColor(Color.);
+		g.fillRect(100, 100, 100, 100);
+	}
+	
+	private void drawHabitat(Graphics g)
+	{
+		g.setColor();
+		g.Rect(100, 100, 100, 100);
+		
+	}
+}
