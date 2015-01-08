05d8c5a3229958917da884993178a169211be6d2
kmccracken5245 <kmccracken5245@001LAB102010540.neisd.ad>
2015-01-08 16:04:20 -0600

    Added Walls

--- ClassZoo8/src/Snowleopard.java
+++ ClassZoo8/src/Snowleopard.java
@@ -27,12 +27,28 @@ public class Snowleopard extends Applet{
 		
 		//Gate
 		g.setColor(new Color(0, 0, 0));
-		g.fillRect(0, 260, 1600, 350);
+		g.fillRect(0, 200, 1600, 300);
 		
 		//Bars in the Gate
 		g.setColor(new Color(4, 186, 255));
-		g.fillRect(15,100 , 1600, 350);
-		
-		
-}
+		g.fillRect(15, 250, 10, 300);
+		g.fillRect(41, 200, 10, 300);
+		g.fillRect(67, 200, 10, 300);
+		g.fillRect(93, 200, 10, 300);
+		g.fillRect(119, 200, 10, 300);
+		g.fillRect(145, 200, 10, 300);
+		g.fillRect(171, 200, 10, 300);
+		g.fillRect(197, 200, 10, 300);
+		g.fillRect(223, 200, 10, 300);
+		g.fillRect(249, 200, 10, 300);
+		g.fillRect(275, 200, 10, 300);
+		g.fillRect(301, 200, 10, 300);
+		g.fillRect(327, 200, 10, 300);
+		g.fillRect(353, 200, 10, 300);
+		g.fillRect(379, 200, 10, 300);
+		g.fillRect(405, 200, 10, 300);
+		g.fillRect(431, 200, 10, 300);
+		g.fillRect(457, 200, 10, 300);
+		g.fillRect(483, 200, 10, 300);
+	}
 }
