c750622bcf14b871d25b6d5f84bd450dd0eb08b8
crodriguezc0697 <crodriguezc0697@001LAB102010544.neisd.ad>
2015-01-09 15:54:02 -0600

    monkey

--- ClassZoo8/src/monkey.java
+++ ClassZoo8/src/monkey.java
@@ -1,4 +1,4 @@
-charlie rodriguez
+//charlie rodriguez
 
 
 import java.awt.*;
@@ -8,17 +8,27 @@ public class monkey extends Applet{
 	
 	public void paint(Graphics g)
 	{
-		drawAnimal(g);
 		drawHabitat(g);
+		drawAnimal(g);
+		
 	}
 
 	private void drawAnimal(Graphics g)
 	{
+		g.setColor(new Color (128,64,0));
+		g.fillOval(500, 100, 300, 200);
+		g.setColor(Color.white);
+		g.fillOval(515, 70, 70, 70);
+		g.fillOval(655, 70, 70, 70);
 		
 	}
 	
 	private void drawHabitat(Graphics g)
 	{
+		g.setColor(new Color (80,180,210));
+		g.fillRect(0, 0, 3000, 500);
+		g.setColor(Color.green);
+		g.fillRect(0, 400, 3000, 500);
 		
 	}
 }
