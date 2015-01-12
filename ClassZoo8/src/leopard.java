3827cfed5a38c8b7f1746803b71a070c05ce1e25
lrivera6310 <lrivera6310@001LAB102010552.neisd.ad>
2015-01-12 16:01:24 -0600

    added leopard

---
+++ ClassZoo8/src/leopard.java
@@ -0,0 +1,26 @@
+/*Computer Science Pre-AP
+8th period
+Joe Dilly
+*/
+
+import java.awt.*;
+import java.applet.*;
+
+public class leopard extends Applet{
+	
+	public void paint(Graphics g)
+	{
+		drawHabitat(g);
+		drawAnimal(g);
+	}
+
+	private void drawAnimal(Graphics g)
+	{
+		
+	}
+	
+	private void drawHabitat(Graphics g)
+	{
+		
+	}
+}
