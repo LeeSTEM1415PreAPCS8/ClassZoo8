97a298c8571d7d849c6e912a4b0e5aca91f43084
lross7849 <lross7849@001LAB102010537.neisd.ad>
2015-01-13 15:51:23 -0600

    fixed a thing with the bun bun bunny

--- ClassZoo8/src/Bunbun.java
+++ ClassZoo8/src/Bunbun.java
@@ -12,19 +12,22 @@ public class Bunbun extends Applet{
 	{
 		//Sky
 		g.setColor(new Color(100,200,255));
-		g.fillRect(0, 0, 1000, 800);
+		g.fillRect(0, 0, 10000, 80000);
 		//Grass
-		g.setColor(new Color(9,164,0));
-		g.fillRect(0,350,1000,1000);
-		g.fillOval(-100, 300, 500, 200);
-		g.fillOval(300, 300, 300, 200);
-		g.fillOval(380, 300, 500, 200);
-		g.fillOval(800, 300, 300, 200);
+			g.setColor(new Color(9,164,0));
+			g.fillRect(0,350,10000,10000);
+			g.fillOval(-100, 300, 500, 200);
+			g.fillOval(300, 300, 300, 200);
+			g.fillOval(380, 300, 500, 200);
+			g.fillOval(800, 300, 300, 200);
 		//Sun Sun
 		g.setColor(Color.yellow);
-		g.fillOval(697,97,80,80);
+			g.fillOval(697,97,80,80);
 		g.setColor(Color.orange);
-		g.fillOval(700, 100, 75, 75);
+			g.fillOval(700, 100, 75, 75);
+		//Tree
+		g.setColor(new Color(77,50,30));
+		
 	}
 	private void drawAnimal(Graphics g)
 	{
@@ -71,40 +74,40 @@ public class Bunbun extends Applet{
 			
 			//White Bun bun
 		g.setColor(Color.white);
-			g.fillOval(248+500,500,50,60);
+			g.fillOval(248+300,500,50,60);
 		g.setColor(Color.gray);
-			g.drawOval(248+500,500,50,60);
-			g.drawOval(253+500, 450, 15, 55);
-			g.drawOval(278+500, 450, 15, 55);
+			g.drawOval(248+300,500,50,60);
+			g.drawOval(253+300, 450, 15, 55);
+			g.drawOval(278+300, 450, 15, 55);
 		g.setColor(Color.white);
-			g.fillOval(253+500,480,40,40);
+			g.fillOval(253+300,480,40,40);
 		g.setColor(Color.gray);
-			g.drawOval(253+500,480,40,40);
+			g.drawOval(253+300,480,40,40);
 		g.setColor(Color.white);
-			g.fillOval(253+500, 450, 15, 55);
-			g.fillOval(278+500, 450, 15, 55);
-			g.fillOval(242+500,515,20,25);
-			g.fillOval(285+500,515,20,25);
-			g.fillOval(240+500,545,30,25);
-			g.fillOval(272+500,545,30,25);
+			g.fillOval(253+300, 450, 15, 55);
+			g.fillOval(278+300, 450, 15, 55);
+			g.fillOval(242+300,515,20,25);
+			g.fillOval(285+300,515,20,25);
+			g.fillOval(240+300,545,30,25);
+			g.fillOval(272+300,545,30,25);
 		g.setColor(Color.gray);
-			g.drawOval(242+500,515,20,25);
-			g.drawOval(285+500,515,20,25);
-			g.drawOval(240+500,545,30,25);
-			g.drawOval(272+500,545,30,25);
+			g.drawOval(242+300,515,20,25);
+			g.drawOval(285+300,515,20,25);
+			g.drawOval(240+300,545,30,25);
+			g.drawOval(272+300,545,30,25);
 		g.setColor(Color.gray);
-			g.drawOval(259+500,489+4,10,1);
-			g.drawOval(277+500,489,10,9);
+			g.drawOval(259+300,489+4,10,1);
+			g.drawOval(277+300,489,10,9);
 		g.setColor(Color.black);
-			g.fillOval(279+500,491,5,5);
-			g.fillOval(277+500,491,5,5);
+			g.fillOval(279+300,491,4,5);
+			g.fillOval(277+300,491,4,5);
 		g.setColor(Color.black);
-			g.drawOval(273+500,498,15,15);
-			g.drawOval(260+500,498,15,15);
+			g.drawOval(273+300,498+1,15,15);
+			g.drawOval(260+300-1,498+1,15,15);
 		g.setColor(Color.white);
-			g.fillRect(257+500, 498, 33,5 );
+			g.fillRect(257+300, 498+1, 33,5 );
 		g.setColor(Color.black);
-			g.fillRect(270+500, 500, 6,6);
+			g.fillRect(270+301, 500, 6,6);
 	}
 	
 
