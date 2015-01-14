dca89e7ce1bc605a40f8a82b11a59e5b7e232041
rlavelle5596 <rlavelle5596@001LAB102010525.neisd.ad>
2015-01-14 15:58:53 -0600

    added the habitat and one bubble

--- ClassZoo8/src/Squid.java
+++ ClassZoo8/src/Squid.java
@@ -21,6 +21,13 @@ public class Squid extends Applet{
 	
 	private void drawHabitat(Graphics g)
 	{
-		g.drawline(100,100);
+		//water
+		g.setColor(new Color(100,200,255));
+		g.fillRect(0, 0, 80000, 80000);
+		
+		//bubbles
+		// oval: X1:distance out on x axis, y1: how far down the sphere is x2: how wide the sphere is y2 how tall the sphere is
+		g.setColor(new Color(255,255,255));
+		g.fillOval(1200,200,50,50);
 	}
 }
