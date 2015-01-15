adb24d6c335b8329ae23c8114efee2d29f1ce87f
rlavelle5596 <rlavelle5596@001LAB102010525.neisd.ad>
2015-01-15 16:00:53 -0600

    Added eyes

--- ClassZoo8/src/Squid.java
+++ ClassZoo8/src/Squid.java
@@ -27,6 +27,22 @@ public class Squid extends Applet{
 		//squid body
 		g.fillRect(600, 350, 200, 300);
 		
+		//squid tenticles
+		g.setColor(Color.white);
+		Polygon s = new Polygon();
+		s.addPoint(600, 800);
+		s.addPoint(500, 900);
+		s.addPoint(525, 925);
+ 
+		g.fillPolygon(s);
+		
+		//squid eyes
+		g.setColor(new Color(0, 0, 0));
+		g.fillOval(610, 400, 75, 75);
+		g.fillOval(710, 400, 75, 75);
+		g.setColor(new Color(255, 255, 255));
+		g.fillOval(640, 400, 25, 25);
+		g.fillOval(740, 400, 25, 25);
 	}
 	
 	private void drawHabitat(Graphics g)
@@ -37,7 +53,7 @@ public class Squid extends Applet{
 		
 		//bubbles
 		// oval: X1:distance out on x axis, y1: how far down the sphere is x2: how wide the sphere is y2 how tall the sphere is
-		g.setColor(new Color(255,255,255));
+		g.setColor(new Color(255, 255, 255));
 		g.fillOval(1200,200,50,50);
 		g.fillOval(1100,500,50,50);
 		
