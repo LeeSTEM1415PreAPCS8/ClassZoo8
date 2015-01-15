b4a5147a1c859974325c35e7a774edae13292bbb
csimnitt8846 <csimnitt8846@001LAB102010549.neisd.ad>
2015-01-15 15:25:40 -0600

    animateed

--- ClassZoo8/src/Pig.java
+++ ClassZoo8/src/Pig.java
@@ -1,49 +1,45 @@
-
-
 import java.awt.*;
 import java.applet.*;
-
-
-
-public class Pig extends Applet
-{
+public class Pig extends Applet implements Runnable {
 	 int x = 300;
 	 int y =	500;
 	public void paint1(Graphics g)
 	{
-		
-		
-		
+		drawHabitat(g);
+		drawAnimal(g);
 	}
 	
-	 Thread runner;
+	Pig drawAnimal;
+	Thread runner;
 		 
-	 public void start() {
-		     if (runner == null) {
-       
+	public void start() {
+	   if (runner == null) {
+	       runner = new Thread(this);
+		   runner.start();
      }
    }
-	// public void stop() {
-	  //   if (runner != null) {
-	    //	 runner.stop();
-	    	// runner= null;
-	   //  }
-	 //}
+//	 public void stop() {
+	//  if (runner != null) {
+		// runner.stop();
+	//runner= null;
+	//}
+	//}
 	 
 	 
 	 public void run() {
-		      while (true) {
-		       repaint(x+5);
-		       //try { Thread.sleep(1); }
-		        //catch (InterruptedException e) { }
+	  while (true) {
+	  x+=5;
+		  repaint();
+	  
+	try { Thread.sleep(750); }
+	catch (InterruptedException e) { }
 		      }
 		    }
 	 public void paint(Graphics g) {
-		     
-		 drawAnimal(g);		    }
-		 
+			drawHabitat(g);  
+			drawAnimal(g);		    
 	 
-	
+	 }
 	private void drawAnimal(Graphics g)
 	{
 		
@@ -56,12 +52,12 @@ public class Pig extends Applet
 		g.fillRect(x+120, y, 20, 15); //nose
 		
 		g.setColor(Color.white);
-		g.fillOval(420, 490, 10, 10); // eye
+		g.fillOval(x+120, y-10, 10, 10); // eye
 
 		g.setColor(Color.black);
-		g.drawLine(320, 550, 320, 588); // left line
-		g.drawLine(390, 550, 390, 588); // right line
-		g.fillOval(425, 492, 5, 5); // pupil
+		g.drawLine(x+20, y+50, x+20, y+88); // left line
+		g.drawLine(x+90, y+50, x+90, y+88); // right line
+		g.fillOval(x+125, y-8, 5, 5); // pupil
 
 	
 
