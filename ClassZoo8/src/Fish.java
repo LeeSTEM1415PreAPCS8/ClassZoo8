a1a4c935e4124be6927f14597d361e76d8be83bb
bvaldez5337 <bvaldez5337@001LAB092006630.neisd.ad>
2015-02-03 16:01:43 -0600

    Bettered the fish

--- ClassZoo8/src/Fish.java
+++ ClassZoo8/src/Fish.java
@@ -18,9 +18,32 @@ import java.awt.Graphics;
 		g.setColor(new Color (254,209,29));
 		g.fillOval(100,100,200,200);
 		//clouds
-		g.setColor(new Color (177,177,177));
-		g.fillOval(500,60,200,100);
-		g.fillOval(1000,120,200,100);
+		g.setColor(new Color(193,193,193));
+		// top right
+		g.drawOval(300, 20, 150, 50); g.fillOval(300, 20, 150, 50);
+		g.drawOval(400, 20, 150, 50); g.fillOval(400, 20, 150, 50);
+		g.drawOval(500, 20, 150, 50); g.fillOval(500, 20, 150, 50);
+		g.drawOval(600, 20, 150, 50); g.fillOval(600, 20, 150, 50);
+		g.drawOval(700, 20, 150, 50); g.fillOval(700, 20, 150, 50);
+		// top left
+		g.drawOval(1000, 20, 150, 50); g.fillOval(1000, 20, 150, 50);
+		g.drawOval(1100, 20, 150, 50); g.fillOval(1100, 20, 150, 50);
+		g.drawOval(1200, 20, 150, 50); g.fillOval(1200, 20, 150, 50);
+		//bottom right
+		g.drawOval(250, 40, 150, 50); g.fillOval(250, 40, 150, 50);
+		g.drawOval(350, 40, 150, 50); g.fillOval(350, 40, 150, 50);
+		g.drawOval(450, 40, 150, 50); g.fillOval(450, 40, 150, 50);
+		g.drawOval(550, 40, 150, 50); g.fillOval(550, 40, 150, 50);
+		g.drawOval(650, 40, 150, 50); g.fillOval(650, 40, 150, 50);
+		g.drawOval(750, 40, 150, 50); g.fillOval(750, 40, 150, 50);
+		//bottom left
+		g.drawOval(950, 40, 150, 50); g.fillOval(950, 40, 150, 50);
+		g.drawOval(1050, 40, 150, 50); g.fillOval(1050, 40, 150, 50);
+		g.drawOval(1150, 40, 150, 50); g.fillOval(1150, 40, 150, 50);
+		g.drawOval(1250, 40, 150, 50); g.fillOval(1250, 40, 150, 50);
+		
+		
+		
 		//ocean
 		g.setColor(new Color (0,56,249));
 		g.fillRect(0,400,1700,400);
@@ -46,7 +69,29 @@ import java.awt.Graphics;
 		g.fillRect(1000,600,20,350);
 		g.fillRect(1200,460,23,700);
 		g.fillRect(1400,600,20,250);
-	}
+		//boat
+		g.setColor(new Color (85,99,80));
+		g.fillArc(700,140,700,400,180,180);
+		g.setColor(new Color (243,7,26));
+		g.fillRect(750,170,600,170);
+		
+		//g.setColor
+		g.fillRect(800,120,50,100);
+		//pollution
+		g.setColor(new Color (46,47,48));
+		g.fillOval(750,100,80,20);
+		g.setColor(new Color (208,209,210));
+		g.fillOval(710,100,80,20);
+		g.fillOval(730,110,80,20);
+		g.setColor(new Color (46,47,48));
+		g.fillOval(780,110,80,20);
+		//window
+		g.setColor(new Color (171,230,252));
+		g.fillRect(1240,190,60,100);
+		g.setColor(new Color (0,0,0));
+		g.fillRect(1265,190,10,100);
+		g.fillRect(1240,235,60,10);
+		}
 	
 	
 }
