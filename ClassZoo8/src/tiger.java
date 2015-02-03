4703aa190e1c4b67d085523cbea6c92e6c6b1017
stijerina2282 <stijerina2282@001LAB102010553.neisd.ad>
2015-02-03 15:53:58 -0600

    added tiger

--- ClassZoo8/src/tiger.java
+++ ClassZoo8/src/tiger.java
@@ -5,16 +5,17 @@ public class tiger extends Applet{
 	
 	public void paint(Graphics g)
 	{
-		drawAnimal(g);
 		drawHabitat(g);
+		drawAnimal(g);
 	}
 
 	private void drawAnimal(Graphics g)
 	{
-		g.setColor(Color.PINK);
-				
-				
-				
+		//tiger
+		g.setColor(new Color(250, 37, 213));
+		g.fillRect(200,400,300,200);
+		g.fillRect(100,350,100,100);
+		
 				
 				
 				
@@ -27,6 +28,26 @@ public class tiger extends Applet{
 	
 	private void drawHabitat(Graphics g)
 	{
+		//sky
+		g.setColor(new Color(100,200,255));
+		g.fillRect(0, 0, 1000, 800);
 		
+		//Grass
+		g.setColor(Color.GREEN);
+		g.fillRect(0,350,1000,1000);
+		g.fillOval(-100, 300, 500, 200);
+		g.fillOval(300, 300, 300, 200);
+		g.fillOval(380, 300, 500, 200);
+		g.fillOval(800, 300, 300, 200);
+			
+		//Sun Sun
+		g.setColor(Color.yellow);
+		g.fillOval(697,97,80,80);
+		g.setColor(Color.orange);
+		g.fillOval(700, 100, 75, 75);
+		}
+				
+	
 	}
-}
+
+
