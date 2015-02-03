077cd756aa6d6b42d788b9f753f21a648f9b04ee
csimnitt8846 <csimnitt8846@001LAB102010549.neisd.ad>
2015-02-03 15:36:11 -0600

    neew colors for pig

--- ClassZoo8/src/Pig.java
+++ ClassZoo8/src/Pig.java
@@ -1,84 +1,93 @@
-import java.awt.*;
-import java.applet.*;
-public class Pig extends Applet implements Runnable {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.GradientPaint;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+class DrawPanel extends JPanel {
 	 int x = 300;
 	 int y =	500;
-	public void paint1(Graphics g)
-	{
-		drawHabitat(g);
-		drawAnimal(g);
-	}
-	
-	Pig drawAnimal;
-	Thread runner;
-		 
-	public void start() {
-	   if (runner == null) {
-	       runner = new Thread(this);
-		   runner.start();
-     }
-   }
-//	 public void stop() {
-	//  if (runner != null) {
-		// runner.stop();
-	//runner= null;
-	//}
-	//}
-	 
-	 
-	 public void run() {
-	  while (true) {
-	  x+=5;
-		  repaint();
-	  
-	try { Thread.sleep(750); }
-	catch (InterruptedException e) { }
-		      }
-		    }
-	 public void paint(Graphics g) {
-			drawHabitat(g);  
-			drawAnimal(g);		    
-	 
-	 }
-	private void drawAnimal(Graphics g)
-	{
-		
-		//g.fillOval(x, y, width, height);
-		g.setColor(Color.pink);
-		g.fillRect(x, y, 100, 50); //body
-		g.fillRect(x, y + 10, 30, 80); //left leg
-		g.fillRect(x +70, y+10, 30, 80); // right leg
-		g.fillRect(x+80, y-20, 50, 50); // head
-		g.fillRect(x+120, y, 20, 15); //nose
-		
-		g.setColor(Color.white);
-		g.fillOval(x+120, y-10, 10, 10); // eye
+	 int a = 139 ;  int b = 69 ; int c = 19;
+    private void doDrawing(Graphics g){ 
+    Graphics2D g2 = (Graphics2D) g;
+{
+        
+        Graphics2D g2d = (Graphics2D) g;
+        //g.setColor(Color.blue);
+        GradientPaint gp1 = new GradientPaint(0, 0, Color.red, 30, 50, Color.blue, true);
+        g2d.setPaint(gp1);
 
-		g.setColor(Color.black);
-		g.drawLine(x+20, y+50, x+20, y+88); // left line
-		g.drawLine(x+90, y+50, x+90, y+88); // right line
-		g.fillOval(x+125, y-8, 5, 5); // pupil
-
-	
-
-	}
-	
-	private void drawHabitat(Graphics g)
-	{
-		int a = 139 ;  int b = 69 ; int c = 19;
-		g.setColor(Color.blue);
-		g.fillRect(0, 0, 9000, 9000);
+        Rectangle2D.Double sky= new Rectangle2D.Double(0, 0, 9000, 9000); g2d.fill(sky);
 		
-		g.setColor(Color.green);
-		g.fillRect(0, 400, 9000, 9000);
+		//g.setColor(Color.green);
+        GradientPaint gp2 = new GradientPaint(0, 250, Color.yellow, 0, 0, Color.green, true);
+        g2d.setPaint(gp2);
+		Rectangle2D.Double land = new Rectangle2D.Double(0, 400, 9000, 9000); g2d.fill(land);
 		
 		g.setColor(new Color (a,b,c));
-		g.fillOval(100, 450, 500, 200);
-//by Connor Simnitt
+		Ellipse2D.Double mudpit = new Ellipse2D.Double(100, 450, 500, 200); g2d.fill(mudpit); 
+		
+        g.setColor(Color.pink);
+        Rectangle2D.Double body= new Rectangle2D.Double(x, y, 100, 50); g2d.fill(body);
+        Rectangle2D.Double legL= new Rectangle2D.Double(x, y + 10, 30, 80); g2d.fill(legL);
+        Rectangle2D.Double legR= new Rectangle2D.Double(x +70, y+10, 30, 80); g2d.fill(legR);
+        Rectangle2D.Double head= new Rectangle2D.Double(x+80, y-20, 50, 50); g2d.fill(head);  
+        Rectangle2D.Double nose= new Rectangle2D.Double(x+120, y, 20, 15); g2d.fill(nose); 
+		
+		g.setColor(Color.white);
+		Ellipse2D.Double eye = new Ellipse2D.Double(x+120, y-10, 10, 10); g2d.fill(eye); g.fillOval(x+120, y-10, 10, 10); // eye
 
+		g.setColor(Color.black);
+		Line2D.Double lineL= new Line2D.Double(x+20, y+50, x+20, y+88);g2d.draw(lineL); // left line
+		Line2D.Double lineR= new Line2D.Double(x+90, y+50, x+90, y+88);g2d.draw(lineR);  // right line
+		Ellipse2D.Double pupil = new Ellipse2D.Double(x+125, y-8, 5, 5); g2d.fill(pupil);  // pupil
+
+	
+}
 	}
-	}
+    
 
+    @Override
+    public void paintComponent(Graphics g) {
+        
+        super.paintComponent(g);
+        doDrawing(g);
+        
+        
+       
+    }
+}
 
+public class Pig extends JFrame {
 
+    public Pig() {
+        initUI();
+    }
 
+    public final void initUI() {
+
+        DrawPanel dpnl = new DrawPanel();
+        add(dpnl);
+
+        setSize(360, 300);
+        setTitle("The Pig in it's Natural Habitat");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Pig ex = new Pig();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
