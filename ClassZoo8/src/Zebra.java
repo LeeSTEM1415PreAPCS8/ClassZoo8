039147384ee2f3666c3f9c6c8dc76e52e17f6c24
chernandez7121 <chernandez7121@001LAB102010550.neisd.ad>
2015-02-05 16:01:42 -0600

    Zebra

--- ClassZoo8/src/Zebra.java
+++ ClassZoo8/src/Zebra.java
@@ -1,81 +1,69 @@
-/*Computer Science Pre-AP
-8th period
-Joe Dilly
-*/
+//Christian Hernandez
 
-import java.awt.*;
-import java.applet.*;
 
-public class Zebra extends Applet{
-	
-	public void paint(Graphics g)
-	{
-		drawHabitat(g);
-		drawAnimal(g);
-	}
+import java.awt.Color;
 
-	private void drawAnimal(Graphics g)
-	{
-		// Body
-		g.setColor(Color.white);
-		g.fillOval(600, 300, 300, 150);
-		g.fillRect(625, 415, 25, 100);
-		g.fillRect(675, 415, 25, 100);
-		g.fillRect(780, 415, 25, 100);
-		g.fillRect(850, 415, 25, 100);
-		// Hooves
-		g.setColor(Color.black);
-		g.fillRect(625, 515, 25, 15);
-		g.fillRect(675, 515, 25, 15);
-		g.fillRect(780, 515, 25, 15);
-		g.fillRect(850, 515, 25, 15);
-		// Neck
-		g.setColor(Color.white);
-		Polygon p = new Polygon();
-		p.addPoint(640, 350);
-		p.addPoint(580, 275);
-		p.addPoint(560, 300);
-		p.addPoint(630, 425);
-		g.fillPolygon(p);
-		//Face
-		g.setColor(Color.white);
-		g.fillOval(495, 265, 100, 50);
-		g.setColor(Color.black);
-		g.fillOval(550, 270, 20, 20);
-		g.setColor(new Color(100, 200, 255));
-		g.fillOval(551, 278, 10, 10);
-		g.setColor(Color.black);
-		g.fillRect(495, 280, 20, 20);
-		g.setColor(new Color(100, 200, 255));
-		g.fillOval(440, 150, 130, 130);
-		g.fillOval(645, 220, 260, 100);
-		g.setColor(Color.black);
-		g.drawLine(510, 308, 530, 295);
-		//Tail
-		g.setColor(Color.white);
-		g.fillRect(890, 360, 125, 20);
-		//ears
-		g.setColor(Color.white);
-		g.fillOval(554, 230, 25, 40);
-		g.fillOval(540, 230, 25, 40);
-		g.setColor(Color.pink);
-		g.fillOval(554, 235, 15, 35);
-		g.fillOval(540, 235, 15, 35);
-		//mane
-		
-		
-	}
-	
-	private void drawHabitat(Graphics g)
-	{
-		//sky
-		g.setColor(new Color(100, 200, 255));
-		g.fillRect(0, 0, 2000, 350);
-		//grass
-		g.setColor(Color.green);
-		g.fillRect(0, 350, 2000, 850);
-		//tree
-		g.setColor(new Color(152, 137, 58));
+import java.awt.GradientPaint;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import java.awt.GradientPaint;
+import java.awt.GradientPaint;
+
+class DrawPanel extends JPanel {
+
+    private void doDrawing(Graphics g) {
+        
+    	   Graphics2D g2d = (Graphics2D) g;
+
+
+        GradientPaint gp6 = new GradientPaint(10, 5,
+                Color.yellow, 20, 10, Color.green, true);
+        	g2d.setPaint(gp6);
+      //  g2d.setColor(new Color(32, 142, 35));
+        g2d.fillRect(0, 500, 2000, 400);
+
+        g2d.setColor(new Color(42, 179, 231));
+        g2d.fillRect(0, 0, 2000, 500);
+
+        //tree
+        g.setColor(new Color(152, 137, 58));
 		g.fillRect(1300, 250, 100, 300);
 		//leaves
 		g.setColor(new Color(6, 179, 109));
@@ -93,9 +81,117 @@ public class Zebra extends Applet{
 		//sun
 		g.setColor(Color.yellow);
 		g.fillOval(25, 50, 100, 100);
-		
-		
-		
-		
-	}
+		// Body
+
+        
+        GradientPaint gp1 = new GradientPaint(5, 20,
+                Color.white, 20, 20, Color.black, true);
+        	g2d.setPaint(gp1);
+        	g2d.fillOval(600, 300, 300, 150);
+        	g2d.fillRect(625, 415, 25, 100);
+			g2d.fillRect(675, 415, 25, 100);
+			g2d.fillRect(780, 415, 25, 100);
+			g2d.fillRect(850, 415, 25, 100);
+			// Hooves
+			g.setColor(Color.black);
+			g.fillRect(625, 515, 25, 15);
+			g.fillRect(675, 515, 25, 15);
+			g.fillRect(780, 515, 25, 15);
+			g.fillRect(850, 515, 25, 15);
+			// Neck
+			  GradientPaint gp2 = new GradientPaint(5, 20,
+		                Color.white, 20, 20, Color.black, true);
+		        	g2d.setPaint(gp2);
+		    Polygon p = new Polygon();
+			p.addPoint(640, 350);
+			p.addPoint(580, 275);
+			p.addPoint(560, 300);
+			p.addPoint(630, 425);
+			g.fillPolygon(p);
+				//Face
+			 GradientPaint gp4 = new GradientPaint(5, 20,
+		                Color.white, 20, 20, Color.black, true);
+		        	g2d.setPaint(gp4);
+				g2d.fillOval(495, 265, 100, 50);
+				g.setColor(Color.black);
+				g.fillOval(550, 270, 20, 20);
+				g.setColor(Color.white);
+				g.fillOval(551, 278, 10, 10);
+				g.setColor(Color.blue);
+				g.fillOval(551, 282, 5, 5);
+				g.setColor(Color.black);
+				g.fillRect(495, 280, 20, 20);
+				g.setColor(new Color(42, 179, 231));
+				g.fillOval(440, 150, 130, 130);
+				g.fillOval(645, 220, 260, 100);
+				g.setColor(Color.red);
+				g.drawLine(510, 308, 530, 295);
+				//Tail
+				  GradientPaint gp3 = new GradientPaint(5, 20,
+			                Color.white, 20, 20, Color.black, true);
+			        	g2d.setPaint(gp3);
+				g.fillRect(890, 360, 125, 20);
+				g.setColor(Color.black);
+				g.fillOval(998, 357, 75, 25);
+				//ears
+				 GradientPaint gp5 = new GradientPaint(5, 20,
+			                Color.white, 20, 20, Color.black, true);
+			        	g2d.setPaint(gp5);
+				g2d.fillOval(554, 230, 25, 40);
+				g2d.fillOval(540, 230, 25, 40);
+				g.setColor(Color.pink);
+				g.fillOval(554, 235, 15, 35);
+				g.fillOval(540, 235, 15, 35);
+				//stripes
+				g.setColor(Color.black);
+				//g.fillRect(670, 308, 20, 75 );
+				//g.fillRect(740, 320, 20, 105 );
+				//g.fillRect(810, 318, 20, 75 );
+				//g.fillRect(700, 373, 20, 75 );
+				//g.fillRect(770, 373, 20, 75 );
+				//g.fillRect(600, 315, 15, 75 );
+				//g.fillRect(625, 450, 15, 10);
+				//g.fillRect(635, 490, 15, 10);
+				//g.fillRect(675, 470, 23, 10);
+				//g.fillRect(780, 470, 15, 10);
+				//g.fillRect(850, 450, 15, 10);
+				//g.fillRect(860, 490, 15, 10);
+				
+				
+				    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        
+        super.paintComponent(g);
+        doDrawing(g);
+    }
+}
+
+public class Zebra extends JFrame {
+
+    public Zebra() {
+        initUI();
+    }
+
+    public final void initUI() {
+
+        DrawPanel dpnl = new DrawPanel();
+        add(dpnl);
+
+        setSize(1700, 900);
+        setTitle("Zebra");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Zebra ex = new Zebra();
+                ex.setVisible(true);
+            }
+        });
+    }
 }
