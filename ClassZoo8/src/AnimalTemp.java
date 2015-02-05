4a90e40cc147eb91ea693fc9f67192b657477c29
Mr. Richard Poore <mrrichpoore@gmail.com>
2015-02-05 15:36:41 -0600

    Changed AnimalTemp

--- ClassZoo8/src/AnimalTemp.java
+++ ClassZoo8/src/AnimalTemp.java
@@ -1,6 +1,8 @@
 /*Computer Science Pre-AP
 8th period
-Joe Dilly
+Your Name - Joe Shmitty or something else . . . whatever your name is
+Really, you need your name so I can grade your work.  You don't want to 
+do work without getting a grade.  You really don't!
 */
 
 import java.awt.*;
